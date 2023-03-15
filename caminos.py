76f7052ff7cfa6061c70cc3ce00bcfa9f0b9df7a
DidoT5 <victor.rn32@gmail.com>
2023-03-15 19:13:50 +0100

    Haciendo bien la clasificacion de camino
    Queda por arreglar parte del algoritmo de clasificacion de caminos, asi como el generador de combinaciones posibles

--- caminos.py
+++ caminos.py
@@ -2,48 +2,87 @@ import numpy as np
 from sympy import *
 from itertools import chain, combinations
 
-def construye_R(t, dos_t, cuatro_t):
-    matriz_R = np.ones( (cuatro_t, cuatro_t), dtype=np.int32)
-    for i in range(1,dos_t):
-        matriz_R[i,(dos_t-i):dos_t] = -1 
-        matriz_R[i,(cuatro_t-i):cuatro_t] = -1
-    for i in range(dos_t,cuatro_t):
-        matriz_R[i,(i-dos_t+1):i+1] = -1
-    return matriz_R.copy()
-
-def es_matriz_hadamard(m, cuatro_t):
-    return np.sum(m[1:cuatro_t,:])==0
-
-def cambia_fila_i(m, i):
-    m[i] = m[i]*(-1)
-    return m.copy()
-
-def calcula_numero_caminos_e_intersecciones(ma, mb, i, t):
+def calcula_intersecciones(fila_i, R_i, t):
     ac = 0
     for j in range(1,4*t):
-        ac += ma[i,j] == mb[i,j] == -1
+        ac += fila_i[j] == R_i[j] == -1
     return ac
 
-def calcula_I_y_c(t, r_i):
-    x, y = symbols('x, y', integer=True)
+def calcula_caminos_combinacion(comb, i, t):
+    caminos = []
+    ciclos = []
+    if type(comb[0]) is tuple:
+        caminos_a = []
+        caminos_b = []
+        #Conjunto a
+        for c in comb[0]:
+            coincidente = (c+i)%(2*t)
+            if coincidente in comb[0]:
+                pertence_camino = False
+                for j in range(len(caminos_a)):
+                    camino = caminos_a[j] 
+                    if c == camino[-1]:
+                        pertence_camino = True
+                        if coincidente != camino[0]:
+                            camino += [coincidente]
+                        else:
+                            camino += [coincidente]
+                            ciclos.append(caminos_a.pop(j))
+                if not pertence_camino:
+                    caminos.append([c,coincidente])
 
-    eq = 2*x - 2*y - (2*t - r_i)
-    #Preguntar porqué c no supera el valor 3 para poder parametrizarlo
-    conds = [(0 <= x), (x < 4), (0 <= y)]
+            else:
+                if all(c != camino[-1] for camino in caminos_a):
+                    caminos_a.append([c])
+        #Conjunto b
+        for c in comb[1]:
+            coincidente = (c+i)%(4*t)
+            if coincidente in comb[0]:
+                pertence_camino = False
+                for j in range(len(caminos_b)):
+                    camino = caminos_b[j] 
+                    if c == camino[-1]:
+                        pertence_camino = True
+                        if coincidente != camino[0]:
+                            camino += [coincidente]
+                        else:
+                            camino += [coincidente]
+                            ciclos.append(caminos_b.pop(j))
+                if not pertence_camino:
+                    caminos.append([c,coincidente])
 
-    # Resolver la ecuación diofantina en el parámetro z_0
-    z, z_0 = symbols('z, z_0', integer=True)
-    gensol = diophantine(eq, z, [x, y])
-    ((xs, ys),) = gensol
+            else:
+                if all(c != camino[-1] for camino in caminos_a):
+                    caminos_b.append([c])
+        caminos = caminos_a + caminos_b
 
-    # Resolver las desigualdades para las restricciones en z_0
-    conds_z = [cond.subs(x, xs).subs(y, ys) for cond in conds]
-    conds_z_sol = solve(conds_z, z_0)
+    else:
+        for c in comb:
+            coincidente = (c+i)%(2*t) if c > 2*t else (c+i)%(2*t)
+            # Si el coincidente esta en las combinaciones posibles
+            if coincidente in comb:
+                pertence_camino = False
+                # Comprobamos si c forma parte de un camino (al estar ordenados solo miramos el ultimo indice)
+                for j in range(len(caminos)):
+                    camino = caminos[j] 
+                    if c == camino[-1]:
+                        pertence_camino = True
+                        # Si el coincidente es igual al primer elemento forma un ciclo y hay que descartarlo de los caminos
+                        if coincidente != camino[0]:
+                            camino += [coincidente]
+                        else:
+                            camino += [coincidente]
+                            ciclos.append(caminos.pop(j))
+                if not pertence_camino:
+                    caminos.append([c,coincidente])
+            # Si ademas de no tener coincidentes, no forma parte de ningun camino, forma uno solo
+            else:
+                if all(c != camino[-1] for camino in caminos):
+                    caminos.append([c])
+    return (caminos, ciclos)
 
-    #Encontrar el conjunto de soluciones finitas para los valores de z_0 
-    set_z0 = (conds_z_sol.as_set() & Integers)
-    sol_set = [(xs.subs(z_0, zi), ys.subs(z_0, zi)) for zi in set_z0]
-    return sol_set
+def es_fila_i_hadamard(c, I_i, t, r_i):
+    return 2*c - 2*I_i - (2*t - r_i) == 0
 
 def obtener_combinaciones_posibles(num_cobordes, t):
     possible_comb_a = []
@@ -61,60 +100,11 @@ def obtener_combinaciones_posibles(num_cobordes, t):
     
     return total
 
-def clasifica_caminos(cobordes, i, t):
-    # Calculo del parametro w
-    num_cobordes = range(t-1,3*t-1)
-    # Contruccion de R
-    R = construye_R(t, t*2, t*4)
-    # Calculo del parametro r_i
-    r_i = np.sum(R[i,:]==-1)
-    # Calculo de los valores c_i,I_i
-    sol_set = calcula_I_y_c(t, r_i)
-    # Actualiza la fila i de cada coborde i para tener dos -1
-    cobordes = [cambia_fila_i(c,j+1) for c,j in zip(cobordes, range(0,len(cobordes)))]
-    # Creamos una lista de todas las posibles combinaciones de cobordes
-    possible_comb = obtener_combinaciones_posibles(num_cobordes, t)
-    matrices_hadamard = []
-    for p in possible_comb:
-        if len(p) < 2:
-            coborde = cobordes[p[0]].copy()
-            interseccion = calcula_numero_caminos_e_intersecciones(coborde.copy(),R.copy(), i, t)
-            if interseccion == 0 and es_matriz_hadamard(np.multiply(coborde.copy(), R.copy()), t*4):
-                matrices_hadamard.append((p[0]+2,coborde.copy()))
-
-        elif type(p[1]) is tuple:
-            if type(p[0]) is tuple:
-                coborde_a = cobordes[p[0][0]].copy()
-                for j in range(1,len(p[0])):
-                    coborde_a = np.multiply(coborde_a.copy(),cobordes[0][j].copy())
-            else:
-                coborde_a = cobordes[p[0]].copy()
-            coborde_b = cobordes[p[1][0]].copy()
-            if len(p[1]) > 1:
-                for j in range(1,len(p[1])):
-                    coborde_b = np.multiply(coborde_b,cobordes[1][j].copy())
-            num_caminos = calcula_numero_caminos_e_intersecciones(coborde_a, coborde_b, i, t)
-            m_final = np.multiply(coborde_a.copy(), coborde_b.copy()).copy()
-            num_intersecciones = calcula_numero_caminos_e_intersecciones(m_final, R, i, t)
-            print("Conjunto a probar: ", p)
-            print("Número de caminos e intersecciones: ",(num_caminos,num_intersecciones), " está en el cojunto:", (num_caminos,num_intersecciones) in sol_set)
-            print("Es matriz de hadamard: ", es_matriz_hadamard(np.multiply(m_final.copy(), R.copy()), t*4))
-            if ((num_caminos, num_intersecciones) in sol_set and es_matriz_hadamard(np.multiply(m_final.copy(), R.copy()), t*4)):
-                matrices_hadamard.append((p, m_final.copy()))
-
-        else:
-            num_caminos = calcula_numero_caminos_e_intersecciones(cobordes[p[0]].copy(), cobordes[p[1]].copy(), i, t)
-            m_final = np.multiply(cobordes[p[0]].copy(),cobordes[p[1]].copy())
-            for j in range(2,len(p)):
-                if calcula_numero_caminos_e_intersecciones(m_final.copy(), cobordes[p[j]].copy(), i, t) == num_caminos:
-                    m_final = np.multiply(m_final.copy(),cobordes[p[j]].copy())
-                else:
-                    num_caminos = -1
-                    break
-            if num_caminos == -1:
-                break
-            elif ((num_caminos, calcula_numero_caminos_e_intersecciones(m_final.copy(), R.copy(), i, t)) in sol_set 
-                and es_matriz_hadamard(np.multiply(m_final.copy(), R.copy()), t*4)):
-                matrices_hadamard.append((p, m_final.copy()))
-
-    return matrices_hadamard
+def clasifica_caminos(comb, cobordes, i, r_i, t, R):
+    (caminos, ciclos) = calcula_caminos_combinacion(comb, i, t)
+    fila_i = cobordes[caminos[0][0]][i,:]
+    c_i = len(caminos)
+    for c in range(1,c_i):
+        fila_i = np.multiply(fila_i,cobordes[c][i,:])
+    I_i = calcula_intersecciones(fila_i, R[i,:], t)
+    return es_fila_i_hadamard(c_i, I_i, t, r_i)
--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 import numpy as np
 from cobordes import genera_cobordes_espacio
-from caminos import clasifica_caminos, construye_R, cambia_fila_i
+from caminos import clasifica_caminos, obtener_combinaciones_posibles
 
 t = 2
-dos_t = t*2
-cuatro_t = t*4
-cobordes = genera_cobordes_espacio(t)
-R = construye_R(t, dos_t, cuatro_t).copy()
-coborde5 = cambia_fila_i(cobordes[3].copy(),4)
-coborde6 = cambia_fila_i(cobordes[4].copy(),5)
 
 def muestra_matriz(m):
     for i in range(0,cuatro_t):
         print(m[i])
 
-muestra_matriz(coborde5)
-muestra_matriz(coborde6)
+def construye_R(t, dos_t, cuatro_t):
+    matriz_R = np.ones( (cuatro_t, cuatro_t), dtype=np.int32)
+    for i in range(1,dos_t):
+        matriz_R[i,(dos_t-i):dos_t] = -1 
+        matriz_R[i,(cuatro_t-i):cuatro_t] = -1
+    for i in range(dos_t,cuatro_t):
+        matriz_R[i,(i-dos_t+1):i+1] = -1
+    return matriz_R.copy()
+
+def cambia_fila_i(m, i):
+    m[i] = m[i]*(-1)
+    return m.copy()
+
+def obtiene_matriz_hadamard(t, comb, cobordes, r_i, R):
+    if all (clasifica_caminos(comb, cobordes.copy(), i, r_i[i-1], t, R.copy()) for i in range(1,t)):
+        return comb
+
+def main(t):
+    # Calculo del parametro w
+    num_cobordes = range(t-1,3*t-1)
+    # Creamos una lista de todas las posibles combinaciones de cobordes
+    possible_comb = obtener_combinaciones_posibles(num_cobordes, t)
+    print(len(possible_comb))
+    # Contruccion de R
+    R = construye_R(t, t*2, t*4)
+    # Actualiza la fila i de cada coborde i para tener dos -1
+    cobordes = genera_cobordes_espacio(t)
+    cobordes = [cambia_fila_i(c,j+1) for c,j in zip(cobordes, range(0,len(cobordes)))]
+    # Calculo del parametro r_i
+    r_i = [np.sum(R[i,:]==-1) for i in range(1,t)]
+    return [comb for comb in possible_comb if obtiene_matriz_hadamard(t, comb, cobordes, r_i, R)]
+        
+print(main(t))
+
 
