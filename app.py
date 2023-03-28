21dced58f67762aa7c166337db82647b2bb8ee0c
DidoT5 <victor.rn32@gmail.com>
2023-03-28 07:12:11 +0200

    Next Calculation implemented

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ from hadamard import Hadarmard
 
 #Create Window object
 app = Tk()
-hadamard = Hadarmard()
+class Calculo:
+    hadamard = None
+    t_value = None
 
 def execute_hadamard():
     t_value = part_text.get()
     try:
-        t_value = int(t_value)
+        Calculo.t_value = int(t_value)
+        Calculo.hadamard = Hadarmard(Calculo.t_value)
         parts_list.delete(0, END)
-        result = hadamard.main(t_value)
+        result = Calculo.hadamard.main(Calculo.t_value)
         for c in result:
             if type(c) is tuple:
                 for valor in c:
@@ -21,20 +24,38 @@ def execute_hadamard():
     except ValueError:
         messagebox.showerror('A value for t is required', 'Must be an integer number')
 
+def next_hadamard():
+    if not(Calculo.hadamard is None) :
+        parts_list.delete(0, END)
+        result = Calculo.hadamard.main(Calculo.t_value)
+        for c in result:
+            if type(c) is tuple:
+                for valor in c:
+                    parts_list.insert(END,valor+2)
+            else:
+                parts_list.insert(END,c+2)
+    else:
+        messagebox.showerror('Hadarmard first search should be executed first')
+
+frame = Frame(app)
 
 part_text = StringVar()
-part_label = Label(app, text='Variable T: ', font=('bold', 16), pady=20)
+part_label = Label(frame, text='Variable T: ', font=('bold', 16), pady=20)
 part_label.grid(column=0, row=0)
-part_entry = Entry(app, textvariable=part_text)
+part_entry = Entry(frame, textvariable=part_text)
 part_entry.grid(row=0, column=1)
 
-parts_list = Listbox(app, height=8, width=50)
+parts_list = Listbox(frame, height=12, width=80)
 parts_list.grid(row=2, column=2, pady=20, padx=20)
 
-start_btn = Button(app, text='Start calculation', width=20, command=execute_hadamard)
+start_btn = Button(frame, text='Start calculation', width=20, command=execute_hadamard)
 start_btn.grid(row=0, column=2, pady=20)
+start_btn = Button(frame, text='Next Combination', width=20, command=next_hadamard)
+start_btn.grid(row=3, column=2, pady=20)
 
 app.title('Hadarmard App')
 app.geometry('800x400')
+
+frame.pack()
 
 app.mainloop()
--- hadamard.py
+++ hadamard.py
@@ -4,8 +4,16 @@ from caminos import clasifica_caminos, obtener_combinaciones_posibles
 
 class Hadarmard:
 
-    def __init__(self):
-        pass
+    def __init__(self, t):
+        self.t = t
+        self.possible_comb = obtener_combinaciones_posibles(range(t-1,3*t-1), t)
+        # Contruccion de R
+        self.R = self.construye_R(t, t*2, t*4)
+        # Actualiza la fila i de cada coborde i para tener dos -1
+        cobordes = genera_cobordes_espacio(t)
+        self.cobordes = [self.cambia_fila_i(c,j+1) for c,j in zip(cobordes, range(0,len(cobordes)))]
+        # Calculo del parametro r_i
+        self.r_i = [np.sum(self.R[i,:]==-1) for i in range(1,t)]
 
     def construye_R(self, t, dos_t, cuatro_t):
         matriz_R = np.ones( (cuatro_t, cuatro_t), dtype=np.int32)
@@ -24,20 +32,10 @@ class Hadarmard:
         return all (clasifica_caminos(comb, cobordes.copy(), i, r_i[i-1], t, R.copy()) for i in range(1,t))
 
     def main(self, t):
-        # Calculo del parametro w
-        num_cobordes = range(t-1,3*t-1)
-        # Creamos una lista de todas las posibles combinaciones de cobordes
-        possible_comb = obtener_combinaciones_posibles(num_cobordes, t)
-        # Contruccion de R
-        R = self.construye_R(t, t*2, t*4)
-        # Actualiza la fila i de cada coborde i para tener dos -1
-        cobordes = genera_cobordes_espacio(t)
-        cobordes = [self.cambia_fila_i(c,j+1) for c,j in zip(cobordes, range(0,len(cobordes)))]
-        # Calculo del parametro r_i
-        r_i = [np.sum(R[i,:]==-1) for i in range(1,t)]
         res = None
-        for comb in possible_comb:
-            if self.obtiene_matriz_hadamard(t, comb, cobordes, r_i, R):
-                res = comb
+        for ind in range(len(self.possible_comb)):
+            if self.obtiene_matriz_hadamard(t, self.possible_comb[ind], self.cobordes, self.r_i, self.R):
+                res = self.possible_comb[ind]
+                self.possible_comb = self.possible_comb[ind+1:]
                 break
         return res
